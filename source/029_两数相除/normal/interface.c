c8a6a59d305d5a89c67f247eb4d27838a0f68004
Zhangmeng <15251763223@163.com>
2020-01-27 18:30:56 +0800

    29. 两数相除 完成了两种方法

---
+++ source/029_两数相除/normal/interface.c
@@ -0,0 +1,37 @@
+#include "../interface.h"
+
+int64 divide_fast(int64 dividend, int64 divisor)
+{
+	int64 rst = 0;
+	while (dividend >= divisor) {
+		rst++;
+		dividend -= divisor;
+	}
+	return rst;
+}
+
+int divide(int dividend, int divisor)
+{
+	int64 my_dividend = dividend;
+	int64 my_divisor = divisor;
+	int64 rst = 0;
+	int flag = 1;
+	
+	if (my_dividend < 0) {
+		flag = -flag;
+		my_dividend = -my_dividend;
+	}
+	if (my_divisor < 0) {
+		flag = -flag;
+		my_divisor = -my_divisor;
+	}
+	rst = divide_fast(my_dividend, my_divisor);
+	
+	if (flag == -1) {
+		rst = -rst;
+	}
+	if (rst > 0x7fffffffLL) {
+		return 0x7fffffff;
+	}
+	return rst;
+}
