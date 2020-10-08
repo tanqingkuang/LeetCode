4da27eccbdf5c56cbb756507e85487706c5ea6b9
Zhangmeng <15251763223@163.com>
2020-10-08 22:00:56 +0800

    对公共副本使用了性能优化方案

--- source/X_XXXXXXX/testcase.c
+++ source/X_XXXXXXX/testcase.c
@@ -3,18 +3,10 @@
 
 void testcase1(void)
 {
-	/*
-	if (rst == excp) {
-		set_console_color(10);
-		printf("%s: OK\n", __FUNCTION__);
-	} else {
-		set_console_color(12);
-		printf("%s: Err rst(%u)!=excp(%u)\n", __FUNCTION__, rst, excp);
-	}
-	*/
+	display_result(TRUE, __FUNCTION__);
 }
 
 void testcase(void)
 {
-
+	testcase1();
 }
